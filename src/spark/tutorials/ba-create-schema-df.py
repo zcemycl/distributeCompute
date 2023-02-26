9a01295fdad3bd672dc6910f9475e08cd0f5ffd7
Yui Chun Leung <leo.leung@faculty.ai>
2023-02-26 20:33:36 +0000

    update

--- src/spark/tutorials/ba-create-schema-df.py
+++ src/spark/tutorials/ba-create-schema-df.py
@@ -1,3 +1,4 @@
+from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
@@ -25,3 +26,22 @@ df.show()
 df.printSchema()
 
 # from txt to df
+conf = SparkConf().setAppName("RDD")
+sc = SparkContext.getOrCreate(conf=conf)
+rdd = sc.textFile("/opt/spark-data/StudentData.csv")
+headers = rdd.first()
+columns = headers.split(',')
+rdd = rdd.filter(lambda x: x!=headers).map(lambda x: x.split(','))
+rdd = rdd.map(lambda x: [
+    int(x[0]), # age
+    x[1], # gender
+    x[2], # name
+    x[3], # course
+    int(x[4]), # roll
+    int(x[5]), # marks
+    x[6] # email
+])
+df_rdd = spark.createDataFrame(rdd, schema=schema)
+df_rdd.show()
+df_rdd.printSchema()
+
