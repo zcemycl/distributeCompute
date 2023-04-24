e86a3e61ca9586a8aa665ba1f8f0520e63656bce
Yui Chun Leung <leo.leung@faculty.ai>
2023-04-24 21:02:42 +0100

    add gtest

---
+++ src/cpp_parallelism/tests/test_aa_detach_join.h
@@ -0,0 +1,5 @@
+#include "aa_detach_join/module.h"
+
+TEST(Detach_Join, dry_run) {
+    
+}
---
+++ src/cpp_parallelism/tests/test_main.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "test_aa_detach_join.h"
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
