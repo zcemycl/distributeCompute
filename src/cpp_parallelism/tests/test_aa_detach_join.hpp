defd9b782161d2a77f7d4bb2dadd487a976c15d2
Yui Chun Leung <leo.leung@faculty.ai>
2023-04-24 21:08:05 +0100

    convert test .h to .hpp

--- src/cpp_parallelism/tests/test_aa_detach_join.hpp
+++ src/cpp_parallelism/tests/test_aa_detach_join.hpp
@@ -1,5 +1,5 @@
 #include "aa_detach_join/module.h"
 
 TEST(Detach_Join, dry_run) {
-    
+    detach_join();
 }
--- src/cpp_parallelism/tests/test_main.cpp
+++ src/cpp_parallelism/tests/test_main.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "test_aa_detach_join.h"
+#include "test_aa_detach_join.hpp"
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
