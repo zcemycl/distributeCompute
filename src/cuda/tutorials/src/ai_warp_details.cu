8011f0ee5209558ca7bcaf84de7e9ef83e5ddd29
zcemycl <yui.leung.15@ucl.ac.uk>
2023-04-29 00:36:17 +0100

    update

--- src/cuda/tutorials/src/ai_warp_details.cu
+++ src/cuda/tutorials/src/ai_warp_details.cu
@@ -15,8 +15,17 @@ __global__ void warp_details() {
     int warp_x = blockDim.x/32 + 1;
     int global_warp_id = threadIdx.x/32 + warp_x*blockIdx.x 
         + warp_x*gridDim.x*blockIdx.y;
+
+    printf("tid : %d, bid.x : %d, bid.y : %d, gid : %d, lwid : %d, gbid : %d, gwid : %d\n",
+        threadIdx.x, blockIdx.x, blockIdx.y, gid, local_warp_id, global_block_id, global_warp_id);
 }
 
 void print_warp_details(){
+    dim3 block(42);
+    dim3 grid(2,2);
 
+    warp_details <<<grid,block>>> ();
+    cudaDeviceSynchronize();
+
+    cudaDeviceReset();
 }
---
+++ src/cuda/tutorials/tests/test_ai_warp_details.hpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include "ai_warp_details/module.cuh"
+
+TEST(Warp_Details, dry_run) {
+    print_warp_details();
+}
