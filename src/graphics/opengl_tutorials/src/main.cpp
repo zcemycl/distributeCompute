62511f803ea3eb060318e0415f481d5160b3c941
zcemycl <yui.leung.15@ucl.ac.uk>
2023-05-27 11:30:17 +0100

    update

--- src/graphics/opengl_tutorials/src/main.cpp
+++ src/graphics/opengl_tutorials/src/main.cpp
@@ -6,7 +6,7 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 
 int main(int argc, char** argv) {
-    test_setup1(argc, argv);
+    // test_setup1(argc, argv);
 
     if (!glfwInit()){
         printf("GLFW initializsation failed!");
@@ -17,7 +17,39 @@ int main(int argc, char** argv) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+
+    if (!mainWindow){
+        printf("GLFW window creation failed!");
+        glfwTerminate();
+        return 1;
+    }
+
+    int bufferWidth, bufferHeight;
+    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+    glfwMakeContextCurrent(mainWindow);
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK){
+        printf("GLEW initialisation failed!");
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return 1;
+    }
+
+    glViewport(0, 0, bufferWidth, bufferHeight);
+
+    while (!glfwWindowShouldClose(mainWindow)) {
+        glfwPollEvents();
+
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(mainWindow);
+    }
 
     return 0;
 }
